f5c0794425a4cfae3a96eae3a68b8588f3a3136d
Julio Lima <julio_flima@hotmail.com>
2021-05-16 19:05:44 -0300

    Remove from brother, because of vercel.

--- packages/shared/eslint-config/.eslintrc.js
+++ packages/shared/eslint-config/.eslintrc.js
@@ -1,41 +1,29 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-    jest: true,
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
   },
-  globals: {
-    document: true,
-    window: true,
-    $: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/standard',
-    'prettier/react',
+  "extends": [
+    "plugin:react/recommended",
+    "standard",
+    "plugin:typescript-eslint/recommended",
+    "prettier:typescript-eslint",
+    "prettier/standard",
+    "prettier/react"
   ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
+    "ecmaVersion": 12,
+    "sourceType": "module"
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'space-before-function-paren': 'off',
-    'react/prop-types': 'off',
-    semi: ['error', 'never'],
-  },
-  settings: {
-    'import/resolver': {},
-    react: {
-      version: 'detect',
-    },
-  },
+  "plugins": ["react", "@typescript-eslint", "prettier"],
+  "rules": {
+    "prettier/prettier": "error",
+    "react/prop-types": "off"
+  }
 }
+
--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,5 +1,32 @@
 const withImages = require('next-images');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 module.exports = withImages({
   fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'svg'],
-  esModule: true,
+  webpack(config, options) {
+    return config;
+  }
 });
+
+module.exports = {
+  webpack(config, options) {
+    const { dev, isServer } = options;
+
+    // Do not run type checking twice:
+    if (dev && isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    }
+
+    return config;
+  }
+};
+
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+      loader: 'url-loader?limit=100000000000000000'
+    });
+    return config;
+  }
+};
